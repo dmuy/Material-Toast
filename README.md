b277f0a14c771ec03215b07824c283be7214c451
Dionlee Uy <dionleeuy@gmail.com>
2018-12-04 18:05:14 +0800

    Version 2.0
    - removed jQuery dependency
    - minor css enhancements
    - updated README

--- README.md
+++ README.md
@@ -5,37 +5,33 @@ A simple plugin to display a material concept toast (alert message).
 
 Larger screen layout:
 
-![alt text](https://lh3.googleusercontent.com/AvVf8iDY9NZz1s7ZAJJACmdTWlJFGkK0JjXJ3rtmk6w6IO9K8tGQfDRtSgc60nAxzNmfprsaNRkChLfkjHkFOQt-T4gOCHuvRIeWAAviqDR5vxCOQC25NnMvgYfTSSKgO7ng8Buvkr0DGiOVVSscaGBAvLFwNYpd1ZpRPz2G2h6gIUrAtogkkX2E_UbzOsBl2TDkGeGgylrpBQM00VIKnk3G0VQtcAvtrVlaNiwqua8FGpmUmWyaEit3Kp3_EfYdf0yOPctNzcwJcOFrQJkyNUIu5UIZ7IRKdTqhuYAX6aQkGbr5b6gQCp8HEM2egh1XXUzOnQWZmHbjtA5FPWajyQXo79qyWr8NeOz2gJ2AO2dJd4cC5sqFLHnO5OqENvzkwb7Je6ZCscrQTCRVLIzCRN2HyLnX9sLyZJ5zE9TkLD5qZ9aiGolyGe1I_TCMqORM6O8lZCpM0w_nbu3j6CJPbD-toogPkz2zFbQiUjuG-UgJNSzeLA4jExMG6Oma3TehRy4JjF53XR75JuYMCt1FSt9M7pcwezp8HmTM29fOaAa44sbyhTe9ksZbe69AFGu7X1Mz7uj7c4btqoq8B61Wao7o1o_d1-LmUWt8X6enSs8SFqBG70Og=w1295-h672-no "MDToast large screen layout")
+![alt text](https://lh3.googleusercontent.com/NXGHyY4MJepzoKrkwoP3om_jX5fax20Ggxr-WGJtmAhQJWqhzvhfoLNYVf8ahu1O9Qp7EIzVUzQ8I3tmmMBQK3bC3swKUldzbiagbO2IDkhTW34bylez5_9TDgSJy1fr__Wr-SMY_AqW52PXqbEI3YQOryuwZ603eUYmGGDbpeayccoMm2tvk2joNfmDT-yO7hkadvNBlxbNFr_VANOJuSKZ_sXePepTRmwb1eXhVZ6RkgBnukxuD7kumz28tT1rbG7zUAJOlcvTcfCraagtUSxys3ECZt0-CtJfY807asWpI3F_htuOfHV0ktAXRlKnUk6_MUwx3DljfS3fUD--Si-6QiB8zWyDgl2S2ZTlWpWeO0uenf1mvTpRLxwX-MsmNt5_67ACe90TjyUKFw_3W5IaogyIuc2LPzrtEN6qw6X7nfJxeP6j9DieVG5poMI3YKmOVZ7FxP9hMVuXkYCOjwGCEY4mz58Ty95tcCSPrm7qk5zG-6_ct8fri5jZ0T-gfq4ErwO8m_qZ2epQkQYYDaqQm0z_gMtZnIrvCDEgVTwmVpvb3F6hjhuNywSRY_sF4hchGh6H7GMF1g16FtfBpfaiA-tVNw6-4DlBTyf42HfvbC1tK58i31tssjk-_maWH7_R4NosSPLgpmXyA8CyYdFG=w1233-h657-no "Material-Toast large screen layout")
 
 Smaller screen layout:
 
-![alt text](https://lh3.googleusercontent.com/4L8Ev0_IN2NmFfnJQWLLK_E2BsI5dwxwblUH27J1I-z6P8LL1fmg0G2x7j4Eetk0Z3xcvk5cxusSr3YpuoQA-eusOO0uWzC4iac79xDsrGC_rh6XohXmnRn24PrJfH7vOTxrYcwIcLo3Y3gmUNl-svYeMAOgoRNWv05h8bOJUezVOQjaaXmIpzc6rELMkN8GtKJg7J64lKT3igzx3oBArME21qBn4oFf8SGUUJRvSogjTSbMt8XIkELClK9b3ZJcFKtWBkHdvYhw0xPtFZCxdiymYHXMmPWz8iy8zixWZEcgjqOK6GFCyZdmPFdbH5E7Eaheo7MbDKRTEXxM8IwpObqYDGrhxRq-k9tMg0tFlbo_VstmL4J90utY06aH-H3QLVS7VwPEayV_GHcgzOrbbgETO7aiOEkwR9Gm34pck27xIDJEitA_vbZIiQeaqAXGCwoYuPuqVuYXO9dvSROuLDeP_zch7Ef8gIh2HlrI5E2pFbni_XpFYMAO66CnuskQbRG8bpSsTKhaaXCTKVbkXvacnvVcvoAd8ZuCwU-SnFY025FPGBbGhiVt73N7BuPWRGOIQSnEt-xUHJYRLwSb0jq5_Vkml8m0trF0942pb37WOl8eDun4=w336-h586-no "MDToast smaller screen layout")
+![alt text](https://lh3.googleusercontent.com/eTzAwRX9FXpZrlrWQfGDTuJxBgh5tLA2sIWu8g428yaAVFvh1UjK6fYT8e-IR6bxGeDmKup1B_Pux7ql-P0JzDh3Cbj98KlXd1WN2ObeMyRdHtbAdvmkJdeKsTFwzmiJ3KisAWCq7GL0_UPHl55cMQRsUkstwe9vwngtcvwN6uZola4PZAOdE-J2NNUJwqeHqfYbxWUFfVC3RFloNOsNitK1NJfik8LvlN6jwLNFU53rP9iac_K6lTEl6t0H34xX81JncVeCkJ_BlhwE62BlDv3N_Ynl_UZQUwfFw2jJnw2rC9Ff8QYKydrAktyjg_fWlWJHjqDnboMMX1M23ijN0hwzDdXg90XeLbgk-nIRHRX8va_3L00DcBcYgSaAhbeme_4BtBr0t2eqzPxJYkKMInbGOjN5wwFQerh-_reV8shzaBL9n1u2UIpPknF6P9W7pbPBlSmrVTkt-uh0-CD0dLg58850X1vv1GSwv11MjVbKjHIce7r5aM7cHbP9ygGKUzhgkG1RMibyEttb6qPLubJ1jIsKP98U8fA1EPMyThLdT-u9W4ce3jG5WiGe0NTbpf73JsAGL2ZI72yY4VM7txDJaMYjiZ1IizC5b_G4vI-GIS52SBkH96r31M_ZB1jkxYZTi4JwIS-xTFT6vlJbyYUZ=w319-h556-no "Material-Toast smaller screen layout")
 
 
 ## How to use
-Make sure you include the jQuery library first. Include `mdtoast.css` and `mdtoast.js` in your html file:
+Include `mdtoast.css` and `mdtoast.js` in your html file:
 ```html
 <link rel="stylesheet" type="text/css" href="mdtoast.css">
 <script type="text/javascript" src="mdtoast.js"></script>
 ```
 
-Call `$.mdtoast()` in your script tag:
+Call `mdtoast()` in your script tag:
 ```javascript
-<script>
-  $(document).ready(function(){
-    $.mdtoast('This is a toast message.'); // Initializes and shows default toast
-  });
-</script>
+mdtoast('This is a toast message.'); // Initializes and shows default toast or with the 'new' keyword - i.e new mdtoast(...)
 ```
 
 ## Options
-You can add options when calling `$.mdtoast()` to fit your needs. Below are the options you can use:
+You can add options when calling `mdtoast()` to fit your needs. Below are the options you can use:
 
 | Option      | Defaut       | Description  |
 | ----------- |--------------|--------------|
 | init        | `false`      | Determines if toast is initialize-only (meaning toast will not show unless `show()` is called |
 | duration    | `5000`       | Determines the toast display duration (in milliseconds). |
-| type        | `default`    | Determines the type of toast to display. Other types in `$.mdtoast.type`: `INFO`, `WARNING`, `SUCCESS`, `ERROR`. <br> Or you can just type these string values: `info`, `warning`, `success`, `error`. |
+| type        | `default`    | Determines the type of toast to display. Other types in `mdtoast`: `INFO`, `WARNING`, `SUCCESS`, `ERROR`. <br> Or you can just type these string values: `info`, `warning`, `success`, `error`. |
 | modal       | `false`      | Determines if toast is modal (pointer events on other elements will be disabled). |
 | interaction | `false`      | Determines if toast requires user interaction to dismiss or has some sort of user interaction button to click. |
 | interactionTimeout | `null` | Determines the toast duration (timeout to dismiss) if `interaction` is set to `true`. This overrides the `duration` option if `interaction` is set to `true`. |
@@ -46,41 +42,39 @@ You can add options when calling `$.mdtoast()` to fit your needs. Below are the 
 ### Usage sample
 Below is an example of storing your toast in a variable for future reuse:
 ```javascript
-<script>
-  $(document).ready(function(){
-    // Initializes default toast with duration of 10 seconds (this will not show the toast since init is set to true)
-    var myToast = $.mdtoast('This is a toast message.', { duration: 10000, init: true }); 
-    // Displays the toast
-    $('#somebutton').click(function(){ myToast.show(); });
-  });
-</script>
+// Initializes default toast with duration of 10 seconds (this will not show the toast since init is set to true)
+var myToast = mdtoast('This is a toast message.', { duration: 10000, init: true }); 
+
+// Displays the toast
+myToast.show();
 ```
+
 Different types of toast:
 ```javascript
-<script>
-  $(document).ready(function(){
-    // Initializes different toasts with duration of 10 seconds
-    $.mdtoast('This is an info toast.', { duration: 10000, type: $.mdtoast.type.INFO });      // or type: 'info'
-    $.mdtoast('This is an error toast.', { duration: 10000, type: $.mdtoast.type.ERROR });    // or type: 'error'
-    $.mdtoast('This is a warning toast.', { duration: 10000, type: $.mdtoast.type.WARNING }); // or type: 'warning'
-    $.mdtoast('This is a success toast.', { duration: 10000, type: $.mdtoast.type.SUCCESS }); // or type: 'success'
-  });
-</script>
+// Initializes different toasts with duration of 10 seconds
+mdtoast('This is an info toast.', { duration: 10000, type: mdtoast.INFO });      // or type: 'info'
+mdtoast('This is an error toast.', { duration: 10000, type: mdtoast.ERROR });    // or type: 'error'
+mdtoast('This is a warning toast.', { duration: 10000, type: mdtoast.WARNING }); // or type: 'warning'
+mdtoast('This is a success toast.', { duration: 10000, type: mdtoast.SUCCESS }); // or type: 'success'
 ```
+
 Specifying toast action:
 ```javascript
-<script>
-  $(document).ready(function(){
-    // Initializes a toast with action (this toast will not dismiss unless 'interactionTimeout' is specified)
-    $.mdtoast('Message archived.', 
-      {
-        type: 'success', 
-        interaction: true, actionText: 'UNDO', 
-        action: function(t){
-          //TODO: Undo codes here...
-          t.hide(); // t is the toast object returned by the callback.
-        }
-      });
-  });
-</script>
+// Initializes a toast with action (this toast will not dismiss unless 'interactionTimeout' is specified)
+mdtoast('Message archived.', {
+  type: 'success', 
+  interaction: true, actionText: 'UNDO', 
+  action: function(t){
+    //TODO: Undo codes here...
+    t.hide(); // t is the toast object returned by the callback.
+  }
+});
 ```
+
+### Remember
+Comment or remove the line shown below in the css file if you already have a link to the Roboto font.
+```css
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500');
+```
+
+Older browsers may need the [classList polyfill](https://developer.mozilla.org/en-US/docs/Web/API/Element/classList) which extends classList support back to IE8 (natively, it’s IE10+).
--- mdtoast.js
+++ mdtoast.js
@@ -1,117 +1,220 @@
-﻿/* -- DO NOT REMOVE --
- * jQuery Material Toast plugin v1.1
+﻿/* !Don't remove this!
+ * Material Toast plugin v2.0
+ * https://github.com/dmuy/Material-Toast
  * 
  * Author: Dionlee Uy
  * Email: dionleeuy@gmail.com
- *
- * Date: Tue Apr 4 2017
- *
- * @requires jQuery
- * -- DO NOT REMOVE --
  */
- if (typeof jQuery === 'undefined') { throw new Error('MDToast: This plugin requires jQuery'); }
- +function ($) {
- 	var MDToast = function (message, options) {
- 		this.animateTime = 250;
- 		this.message = message;
- 		this.options = options;
- 		this.toastOpenClass = "mdtoast--open";
- 		this.toastModalClass = "mdtoast--modal";
- 		this.TOAST_DATA = "dmu-md-toast";
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define('mdtoast', [], factory(root));
+	} else if (typeof exports === 'object') {
+		// CommonJS
+		module.exports = factory(root);
+	} else {
+		// Browser globals
+		root.mdtoast = factory(root);
+	}
+})(typeof global !== 'undefined' ? global : this.window || this.global, function (root) {
+	'use strict';
 
- 		this.toast = $('<div class="mdtoast mdt--load"></div>');
- 		this.action = $('<button class="mdt-action" tabindex="0"></button>');
- 		this.toastTimeout = null;
+	var supports = !!document.querySelector && !!root.addEventListener, // feature test
+		defaults = {
+			init: false,				// true if initalize only, false to automatically show toast after initialization.
+			duration: 5000,				// duration ot toast message.
+			type: 'default',			// type of toast to display (can also be info, error, warning, success)
+			modal: false,				// true if you want to disable pointer events when toast is shown
+			interaction: false,			// determines if toast requires user interaction to dismiss
+			interactionTimeout: null,	// if requires interaction, set the value for automatic dismissal of toast (e.g. 2000 -> 2 seconds)
+			actionText: 'OK',			// if requires interaction, set the value like 'UNDO'
+			action: function (t) {		// callback action for the user interaction, hides toast by default
+				t.hide();
+			},
+			callbacks: {}				// callback object for toast; contains hidden() and shown()
+		}, mdtoast = function () {
 
- 		this.toast.addClass(options.type === 'default' ? '' : 'mdt--' + options.type)
- 			.append('<div class="mdt-message">' + message + '</div>');
+			if (!(this instanceof mdtoast))
+				return mdtoast.apply(Object.create(mdtoast.prototype), arguments);
 
- 		if (options.interaction) {
- 			var that = this;
- 			this.action.text(options.actionText)
-	 			.on('click', function () {
-	 				if (options.action) options.action(that);
-	 			});
- 			this.toast.append(this.action).addClass('mdt--interactive');
- 		}
+			var _ = this, args = arguments;
 
- 		if (!options.init) this.show();
- 	}
+			_.animateTime = 250;
+	 		_.message = args[0];
+	 		_.options = extendObj(defaults, args[1]);;
+	 		_.toastOpenClass = "mdtoast--open";
+	 		_.toastModalClass = "mdtoast--modal";
+	 		_.toastTimeout = null;
 
- 	MDToast.prototype = {
- 		constructor : MDToast,
+			if (!_.options.init) buildUI.apply(_);
 
- 		show : function () {
- 			var that = this, callbacks = that.options.callbacks, existingToast = $('.mdtoast'), doc = $('body');
+			return _;
+		}, setAttributes = function(el, attrs) {
+			/* src: http://jsfiddle.net/andr3ww/pvuzgfg6/13/ */
+			var recursiveSet = function(at, set) {
+				for (var prop in at) {
+					var a = at[prop];
+					if ((typeof a === 'object' && a !== null) && a.dataset === undefined && a[0] === undefined) { recursiveSet(a, set[prop]); }
+					else { set[prop] = a; }
+				}
+			}
+			recursiveSet(attrs, el);
+		}, removeSpace = function (str) {
+			return str.replace(/\s+/g,'');
+		}, inArray = function (arr, item) {
+			if (!arr) return false;
+			if (arr[0] === undefined) return false;
 
- 			if (that.toast.is(':visible')) return;
+			return arr.filter(function (x) { return x === item  }).length > 0
+		}, buildUI = function() {
 
- 			that.toast.data(that.TOAST_DATA, that).appendTo(doc);
+			if (!supports) return;
 
- 			setTimeout(function () {
- 				that.toast.removeClass('mdt--load');
+			var _ = this, _options = _.options, _callbacks = _options.callbacks, content, actionBtn,
+				// createElement helper
+				createElem = function(tag, className, content, isHtml) {
+					var el = document.createElement(tag);
 
- 				if (existingToast.length > 0) {
- 					existingToast.each(function () {
- 						var ex_toast = $(this).data(that.TOAST_DATA);
+					el.className = className;
+					
+					if (typeof content !== 'undefined')
+						el[isHtml || false ? 'innerHTML' : 'innerText'] = content;
 
- 						ex_toast.hide();
- 					});
- 				}
+					return el;
+				},
+				// global event handler
+				evtHandler = function (e) {
+					if (e.target.matches('.mdt-action')) {
+						if ((e.type === 'click' || (e.type === 'keypress' && e.keyCode === 13)) && _options.action)
+							_options.action(_);
+					}
+				};
 
- 				setTimeout(function () { if (callbacks && callbacks.shown) callbacks.shown(that); }, that.animateTime);
+			_.docFrag = document.createDocumentFragment();
+			_.toast = createElem('div', 'mdtoast mdt--load');
+			_.toast.tabIndex = 0;
+	 		_.docFrag.appendChild(_.toast);
 
- 				if (that.options.interaction) {
- 					if (that.options.interactionTimeout)
- 						that.toastTimeout = setTimeout(function () { that.hide(); }, that.options.interactionTimeout);
- 				} else if (that.options.duration){
- 					that.toastTimeout = setTimeout(function () { that.hide(); }, that.options.duration);
- 				}
+			if (_options.type !== 'default') 
+				_.toast.classList.add('mdt--' + _options.type);
 
- 				
- 				doc.addClass(that.toastOpenClass);
+			content = createElem('div', 'mdt-message', _.message, true);
+			_.toast.appendChild(content);
 
- 				if (that.options.modal) doc.addClass(that.toastModalClass);
- 			}, 10);
- 		},
- 		hide: function () {
- 			var that = this, callbacks = that.options.callbacks, doc = $('body');
+	 		actionBtn = createElem('span', 'mdt-action');
 
- 			clearTimeout(that.toastTimeout);
+	 		if (_options.interaction) {
+	 			actionBtn.innerText = _options.actionText;
+	 			actionBtn.tabIndex = 0;
+	 			_.toast.classList.add('mdt--interactive');
+	 			_.toast.appendChild(actionBtn);
+	 		}
 
- 			that.toast.addClass('mdt--load');
-            doc.removeClass(that.toastOpenClass).removeClass(that.toastModalClass);
-            setTimeout(function () {
-                that.toast.remove();
-                if(callbacks && callbacks.hidden) callbacks.hidden();
-            }, that.animateTime);
- 		}
- 	}
-	
- 	$.mdtoast = function(message, options) {
- 		return new MDToast(message, $.extend({}, $.mdtoast.defaults, null, typeof options === 'object' && options));
- 	}
+	 		_.toast.addEventListener('click', evtHandler, false);
+	 		_.toast.addEventListener('keypress', evtHandler, false);
 
- 	$.mdtoast.defaults = {
- 		init: false,				// true if initalize only, false to automatically show toast after initialization.
-		duration: 5000,				// duration ot toast message.
-		type: 'default',			// type of toast to display (can also be info, error, warning, success)
-		modal: false,				// true if you want to disable pointer events when toast is shown
-		interaction: false,			// determines if toast requires user interaction to dismiss
-		interactionTimeout: null,	// if requires interaction, set the value for automatic dismissal of toast (e.g. 2000 -> 2 seconds)
-		actionText: 'OK',			// if requires interaction, set the value like 'UNDO'
-		action: function (data) {	// callback action for the user interaction, hides toast by default
-			data.hide();
-		},
-		callbacks: {}				// callback object for toast; contains hidden() and shown()
-	};
+	 		_.toast.mdtoast = _;
 
-	$.mdtoast.type = {
-		INFO : 'info',
-		ERROR : 'error',
-		WARNING : 'warning',
-		SUCCESS : 'success'		
-	};
+			if (!_.options.init) _.show();
+		};
 
-	// $.mdtoast.Constructor = MDToast;
- }(jQuery);
+	Object.defineProperties(mdtoast, {
+		INFO : { value: 'info' },
+		ERROR : { value: 'error' },
+		WARNING : { value: 'warning' },
+		SUCCESS : { value: 'success' }
+	});
+
+	/* Shows toast */
+	mdtoast.prototype.show = function() {
+		var _ = this, callbacks = _.options.callbacks, exToast = document.getElementsByClassName('mdtoast'), doc = document.body;
+
+		if (doc.contains(_.toast)) return;
+
+		if (_.options.init) buildUI.apply(this);
+
+		for (var i = exToast.length - 1; i >= 0; i--) {
+			exToast[i].mdtoast.hide();
+		}
+
+		doc.appendChild(_.docFrag);
+
+		setTimeout(function () {
+			_.toast.classList.remove('mdt--load');
+
+			setTimeout(function () { 
+				if (callbacks && callbacks.shown) callbacks.shown(_);
+
+				_.toast.focus(); 
+			}, _.animateTime);
+
+			if (_.options.interaction) {
+				if (_.options.interactionTimeout)
+					_.toastTimeout = setTimeout(function () { _.hide() }, _.options.interactionTimeout);
+			} else if (_.options.duration)
+				_.toastTimeout = setTimeout(function () { _.hide() }, _.options.duration);
+			
+			doc.classList.add(_.toastOpenClass);
+
+			if (_.options.modal) 
+				doc.classList.add(_.toastModalClass);
+		}, 10);
+	}
+
+	/* Hides toast */
+	mdtoast.prototype.hide = function() {
+		var _ = this, callbacks = _.options.callbacks, doc = document.body;
+
+		clearTimeout(_.toastTimeout);
+
+		_.toast.classList.add('mdt--load');
+        doc.classList.remove(_.toastOpenClass);
+        doc.classList.remove(_.toastModalClass);
+        setTimeout(function () {
+            doc.removeChild(_.toast);
+            if(callbacks && callbacks.hidden) callbacks.hidden();
+        }, _.animateTime);
+	}
+
+	/*
+	* Vanilla JavaScript version of jQuery.extend()
+	* src: https://gomakethings.com/vanilla-javascript-version-of-jquery-extend/
+	*/
+	function extendObj() {
+		// Variables
+		var extended = {};
+		var deep = false;
+		var i = 0;
+		var length = arguments.length;
+
+		// Check if a deep merge
+		if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+			deep = arguments[0];
+			i++;
+		}
+
+		// Merge the object into the extended object
+		var merge = function (obj) {
+			for ( var prop in obj ) {
+				if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+					// If deep merge and property is an object, merge properties
+					if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+						extended[prop] = extend( true, extended[prop], obj[prop] );
+					} else {
+						extended[prop] = obj[prop];
+					}
+				}
+			}
+		};
+
+		// Loop through each object and conduct a merge
+		for ( ; i < length; i++ ) {
+			var obj = arguments[i];
+			merge(obj);
+		}
+
+		return extended;
+	}
+
+	return mdtoast;
+});
